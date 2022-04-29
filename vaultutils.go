95cf85b7bc321dcd197c9b9731b6c1663d23afc5
Avery Carty <avery.carty7@gmail.com>
2022-04-28 20:50:30 -0400

    debugging: auth

--- vaultutils.go
+++ vaultutils.go
@@ -40,7 +40,7 @@ func Auth(req handler.Request, vaultEngine, functionURL string) error {
 	err = json.Unmarshal(b, &m)
 
 	if m[email] != token {
-		return fmt.Errorf(fmt.Sprintf("vault auth: Unauthorized Access\nb:%v\nm:%v~~token:%v", string(b), m, token))
+		return fmt.Errorf(fmt.Sprintf("vault auth: Unauthorized Access\nb:%v\nm:%v~~token:%v\n\n%v", string(b), m, token, err))
 	}
 	return nil
 }
